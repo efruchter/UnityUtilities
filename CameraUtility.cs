8cc3bef52a07d94b57c34e05183a298983ad842d
kazoo <fruchter.eric@gmail.com>
2017-11-22 11:05:55 -0800

    Create CameraUtility.cs

---
+++ CameraUtility.cs
@@ -0,0 +1,10 @@
+public class static CameraUtility
+{
+  // Generate a projection matrix such that the near clip plane is aligned with an arbitrary transform.
+  protected static Matrix4x4 GetObliqueMatrix(this Camera inPortalCamera, Transform inClipPlane)
+  {
+      Vector4 clipPlaneWorldSpace = new Vector4(inClipPlane.forward.x, inClipPlane.forward.y, inClipPlane.forward.z, Vector3.Dot(inClipPlane.position, -inClipPlane.forward));
+      Vector4 clipPlaneCameraSpace = Matrix4x4.Transpose(Matrix4x4.Inverse(inPortalCamera.worldToCameraMatrix)) * clipPlaneWorldSpace;
+      return inPortalCamera.CalculateObliqueMatrix(clipPlaneCameraSpace);
+  }
+}
