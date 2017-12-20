2d37d0a7ac922b282b0c0f5b9816d65d934a4757
kazoo <fruchter.eric@gmail.com>
2017-12-20 10:29:42 -0800

    Update Singleton.cs

--- Patterns/Singleton.cs
+++ Patterns/Singleton.cs
@@ -1,43 +1,67 @@
 using System;
-using System.Collections.Generic;
 using UnityEngine.Assertions;
 
-/// <summary>
-/// Singleton manager. Provides a warning when trying to double-add or get null singletons.
-/// </summary>
-public static class Singleton<T> where T : class
+namespace Patterns
 {
-	private static T _singleton;
-	private static Action<T> _onAdd;
-
-	public static T Instance
+	/// <summary>
+	/// Singleton manager. Provides a warning when trying to double-add or get null singletons.
+	/// </summary>
+	public static class Singleton<T> where T : class
 	{
-		set
+		private static T _singleton;
+		private static Action<T> _onAdd;
+
+		/// <summary>
+		/// O(1) Fetch or add of singleton.
+		/// </summary>
+		public static T Instance
 		{
-			AddSingleton (value);
+			set { AddSingleton(value); }
+			get { return GetSingleton(); }
 		}
-		get
+
+		public static bool Exists
 		{
-			return GetSingleton();
+			get
+			{
+				return _singleton != null;
+			}
 		}
-	}
 
-	public static T GetSingleton()
-	{
-		Assert.IsNotNull (_singleton, "SingletonManager: Warning, attempting to get a singleton that does not exist." +
-			" Be sure you are not calling get before Awake()");
-
-		return _singleton;
-	}
-
-	public static void GetSingletonDeferred(Action<T> onAdd)
-	{
-		Assert.IsNotNull(onAdd, "Callback can not be null.");
-
-		T singleton = GetSingleton();
-		if (singleton == null)
+		/// <summary>
+		/// Get a singleton. Warning if not available.
+		/// </summary>
+		/// <returns>The singleton if available.</returns>
+		public static T GetSingleton()
 		{
-			if (_onAdd == null)
+			Assert.IsTrue(Exists, "Singleton does not exist at this moment. Advice: Fetch Singletons with GetSingletonDeferred, or later in the frame.");
+			return _singleton;
+		}
+
+		/// <summary>
+		/// Perform an action as soon as the singleton is available.
+		/// </summary>
+		public static Action<T> InstanceReady
+		{
+			set
+			{
+				GetSingletonDeferred(value);
+			}
+		}
+
+		/// <summary>
+		/// Get a singleton whenever it is available.
+		/// </summary>
+		/// <param name="onAdd">The callback to run when singleton Exists</param>
+		public static void GetSingletonDeferred(Action<T> onAdd)
+		{
+			Assert.IsNotNull(onAdd, "Callback can not be null.");
+
+			if (Exists)
+			{
+				onAdd(GetSingleton());
+			}
+			else if (_onAdd == null)
 			{
 				_onAdd = onAdd;
 			}
@@ -46,28 +70,46 @@ public static class Singleton<T> where T : class
 				_onAdd += onAdd;
 			}
 		}
-		else
+
+		/// <summary>
+		/// Register a singleton. Replacement is illegal.
+		/// </summary>
+		/// <param name="singleton">The instance.</param>
+		public static void AddSingleton(T singleton)
 		{
-			onAdd(singleton);
+			AddSingleton(singleton, false);
 		}
-	}
 
-	public static void AddSingleton(T singleton)
-	{
-		AddSingleton(singleton, false);
-	}
-
-	public static void AddSingleton(T singleton, bool allowReplace)
-	{
-		Assert.IsNotNull (singleton, "Singleton can not be null.");
-		Assert.IsFalse ((!allowReplace) && (_singleton != null), "Attempting to replace a singleton that already exists.");
-
-		_singleton = singleton;
-
-		if (_onAdd != null)
+		/// <summary>
+		/// Release the singleton so others can use it.
+		/// </summary>
+		public static void Release()
 		{
-			_onAdd (singleton);
+			_singleton = null;
 			_onAdd = null;
+		}
+
+		/// <summary>
+		/// Register a singleton.
+		/// </summary>
+		/// <param name="singleton">The instance.</param>
+		/// <param name="allowReplace">If true, allow replacement without warning.</param>
+		public static void AddSingleton(T singleton, bool allowReplace)
+		{
+			if (singleton == null)
+			{
+				Release();
+				return;
+			}
+
+			Assert.IsFalse(!allowReplace && Exists, "Attempting to replace a singleton that already exists.");
+
+			_singleton = singleton;
+			if (_onAdd != null)
+			{
+				_onAdd(singleton);
+				_onAdd = null;
+			}
 		}
 	}
 }
